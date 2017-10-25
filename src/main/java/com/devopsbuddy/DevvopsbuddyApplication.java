3eceecc195635730537cac5a46284fcff29170d6
Princedavinder <rockdavinder@gmail.com>
2017-10-26 00:41:16 +0530

    initial Project commit

---
+++ src/main/java/com/devopsbuddy/DevvopsbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvopsbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvopsbuddyApplication.class, args);
+	}
+}
